1e8b37d9bb181bc8d6be0dcaf4c2c700c7b501a2
datsfilipe <datsfilipe.foss@proton.me>
2023-09-27 07:17:05 -0300

    feat: add db package

---
+++ pkg/db/db.go
@@ -0,0 +1,15 @@
+package db
+
+import (
+	"database/sql"
+	"log"
+)
+
+func Open() (*sql.DB, error) {
+	connsStr := "postgres://rinha:rinha@db/rinha?sslmode=disable"
+	db, err := sql.Open("postgres", connsStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return db, err
+}
