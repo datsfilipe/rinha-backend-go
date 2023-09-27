40b60a33029368bcadb7c42db06da3a15ce0bfff
datsfilipe <datsfilipe.foss@proton.me>
2023-09-27 07:50:03 -0300

    feat: add pessoas route

--- pkg/routes/router.go
+++ pkg/routes/router.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/datsfilipe/rinha-backend-go/pkg/handlers"
 )
 
 func SetupRouter() *gin.Engine {
@@ -11,6 +12,15 @@ func SetupRouter() *gin.Engine {
 
 	r.GET("/ping", func(c *gin.Context) {
 		c.String(http.StatusOK, "pong")
+	})
+
+	r.POST("/pessoas", func(c *gin.Context) {
+		contextBody, _ := c.GetRawData()
+		response, err := handlers.CreatePersonHandler(contextBody)
+		if err != nil {
+			c.String(http.StatusBadRequest, err.Error())
+		}
+		c.String(http.StatusOK, string(response))
 	})
 
 	return r
