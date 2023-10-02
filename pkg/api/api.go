881615ff8ddf48b811e9d28aa7b25b9f828ba13b
datsfilipe <datsfilipe.foss@proton.me>
2023-10-02 18:52:30 -0300

    refactor: router -> api and some minor changes
    - receiving db as a parameter in the api and handlers
    - adding location, that should be returned from createPerson handler, in
      the headers response

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -1,44 +1,52 @@
-package routes
+package api
 
 import (
-	"github.com/gin-gonic/gin"
+	"database/sql"
+
 	"github.com/datsfilipe/rinha-backend-go/pkg/handlers"
+	"github.com/gin-gonic/gin"
 )
 
-func SetupRouter() *gin.Engine {
-	r := gin.Default()
+func SetupRouter(db *sql.DB) *gin.Engine {
+	r := gin.New()
+	r.Use(gin.Recovery())
 
 	r.POST("/pessoas", func(c *gin.Context) {
 		contextBody, _ := c.GetRawData()
-		response, status, err := handlers.CreatePersonHandler(contextBody)
+		response, status, err, location := handlers.CreatePersonHandler(db, contextBody)
 		if err != nil {
 			c.String(status, "")
 		}
+		c.Header("Location", location)
 		c.String(status, string(response))
 	})
 
 	r.GET("/pessoas/:id", func(c *gin.Context) {
 		id := c.Param("id")
-		response, status, err := handlers.GetPersonHandler(id)
+
+		response, status, err := handlers.GetPersonHandler(db, id)
 		if err != nil {
 			c.String(status, "")
+			return
 		}
 		c.String(status, string(response))
 	})
 
 	r.GET("/pessoas", func(c *gin.Context) {
 		t := c.Query("t")
-		response, status, err := handlers.SearchByTermHandler(t)
+		response, status, err := handlers.SearchByTermHandler(db, t)
 		if err != nil {
 			c.String(status, "")
+			return
 		}
 		c.String(status, string(response))
 	})
 
 	r.GET("/contagem-pessoas", func(c *gin.Context) {
-		response, status, err := handlers.CountPeopleHandler()
+		response, status, err := handlers.CountPeopleHandler(db)
 		if err != nil {
 			c.String(status, "")
+			return
 		}
 		c.String(status, string(response))
 	})
