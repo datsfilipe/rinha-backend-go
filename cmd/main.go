580381ada8d72c0470e31af098a2ce9c0cf387da
datsfilipe <datsfilipe.foss@proton.me>
2023-10-02 19:01:53 -0300

    refactor: setup http server and open db in main

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,28 @@
 package main
 
-import "github.com/datsfilipe/rinha-backend-go/pkg/routes"
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/datsfilipe/rinha-backend-go/pkg/api"
+	"github.com/datsfilipe/rinha-backend-go/pkg/database"
+)
 
 func main() {
-	r := routes.SetupRouter()
+	db, err := database.Open()
+	if err != nil {
+		log.Printf("Error opening database: %v", err)
+	}
 
-	r.Run(":80")
+	router := api.SetupRouter(db)
+
+	server := &http.Server{
+		Addr:           ":80",
+		Handler:        router,
+		ReadTimeout:    1200 * time.Millisecond,
+		WriteTimeout:   1200 * time.Millisecond,
+	}
+	
+	server.ListenAndServe()
 }
